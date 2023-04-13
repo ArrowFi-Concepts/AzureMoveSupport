fca438889407b52a38bdedd3cb6ede4be6330f39
TuukkaArrow <69797126+TuukkaArrow@users.noreply.github.com>
2023-04-13 12:20:23 +0300

    Update AzureMoveCheck1.1.ps1

--- AzureMoveCheck1.1.ps1
+++ AzureMoveCheck1.1.ps1
@@ -4,6 +4,7 @@ Invoke-WebRequest -Uri $uri -OutFile .\moveSupported.csv
 $moveSupported = Import-Csv -Path .\moveSupported.csv
 $Azureresources = Import-Csv -Path .\Azureresources.csv
 "Resource Name,Resource Type,Move Subscription" |Out-File -FilePath .\results.csv
+"Resource Name,Resource Type,Move Subscription,Notes" |Out-File -FilePath .\issues.csv
 foreach($resource in $Azureresources){
     foreach($support in $moveSupported){
         $test = $support.Resource.ToLower()
@@ -12,15 +13,15 @@ foreach($resource in $Azureresources){
             $resource.name +","+$resource."Resource Type" +","+ $support."Move Subscription" | Out-File -FilePath .\results.csv -Append
             #if 0 write to issues.csv
             if(($support."Move Subscription") -eq 0){
-                $resource.name +","+$resource."Resource Type" +","+ $support."Move Subscription" | Out-File -FilePath .\issues.csv -Append
+                $resource.name +","+$resource."Resource Type" +","+ $support."Move Subscription"+"," | Out-File -FilePath .\issues.csv -Append
             }
             #if virtualnetworkgateways write to issues.csv
             if($resource."Resource Type" -like "*virtualnetworkgateways*"){
-                $resource.name +","+$resource."Resource Type" +","+ $support."Move Subscription" + " Yes except Basic SKU (https://learn.microsoft.com/fi-fi/azure/azure-resource-manager/management/move-limitations/networking-move-limitations)"| Out-File -FilePath .\issues.csv -Append
+                $resource.name +","+$resource."Resource Type" +","+ $support."Move Subscription" + ",Yes except Basic SKU (https://learn.microsoft.com/fi-fi/azure/azure-resource-manager/management/move-limitations/networking-move-limitations)"| Out-File -FilePath .\issues.csv -Append
             }
             #if loadbalancers write to issues.csv
             if($resource."Resource Type" -like "*loadbalancers*"){
-                $resource.name +","+$resource."Resource Type" +","+ $support."Move Subscription" + " Yes - Basic SKU, No - Standard SKU"| Out-File -FilePath .\issues.csv -Append
+                $resource.name +","+$resource."Resource Type" +","+ $support."Move Subscription" + ",Yes - Basic SKU # No - Standard SKU"| Out-File -FilePath .\issues.csv -Append
             }
 
         }
